cff846619908ad2d4f69b4560778a20e1c958f06
Jakob Olsson <53495529+maskenjesper@users.noreply.github.com>
2024-11-05 18:00:54 +0100

    improve sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+
+# nix requires all files in the directory to be either commited or staged
+git add --all
+
 sudo nixos-rebuild switch --flake .#desktop-nixos
 home-manager switch --flake . -b backup
