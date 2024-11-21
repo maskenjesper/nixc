6063074645d0e666b9614add6b4f16333a5b72b0
jakob <noreply@email.com>
2024-11-21 20:21:05 +0100

    stuff

--- src/users/modules/features/terminal/tmux/tmux-sessionizer.sh
+++ src/users/modules/features/terminal/tmux/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/nixc ~/todo ~/source ~/.config -mindepth 0 -type d | fzf)
+    selected=$(find ~/nixc ~/notes ~/todo ~/source ~/.config -mindepth 0 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
