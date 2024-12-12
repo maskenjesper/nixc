12f20c74acfca82b32da93b26ddf9d5ca106fd42
jakob <noreply@email.com>
2024-12-13 00:37:32 +0100

    4 current   2024-12-13 00:23:34  25.05.20241119.23e89b7   6.6.62                          *

---
+++ tasks/just/deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+sudo echo starting
+
+pushd ~/nixc/ || exit
+
+
+# nix requires all files in the directory to be either commited or staged
+git add --all
+
+echo ====================== Running home-manager ======================
+home-manager switch --impure --flake . -b backup --show-trace -L -v
+
+if [[ $? -eq 0 ]]; then
+    echo ====================== Running nixos-rebuild ======================
+    sudo nixos-rebuild switch --flake .#"$1" --show-trace -L -v 
+    
+    if [[ $? -eq 0 ]]; then
+        echo Sync successful 
+        gen=$(nixos-rebuild list-generations | grep current)
+        git commit -m "$gen"
+    else
+        echo Rebuild failed. Aborting...    
+    fi
+else
+    echo Home manager failed. Aborting...
+fi
+
+popd || exit
