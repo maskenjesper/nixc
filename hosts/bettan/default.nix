59f1a890f1d05a013630556a99123023fb3bbdd5
jakob <noreply@email.com>
2024-12-15 10:48:04 +0100

    12 current  2024-12-15 01:13:57  25.05.20241119.23e89b7  6.6.62                          *

--- hosts/bettan/default.nix
+++ hosts/bettan/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ../common/global
@@ -12,6 +12,61 @@
   ####### define options values here ########
 
   ###########################################
+
+  environment.systemPackages = with pkgs; [
+    #kicad
+
+    killall
+    usbimager
+
+    #zed-editor
+
+    #bash-language-server # temp
+
+    # Gnome stuff
+    pomodoro-gtk
+    blanket
+
+    nix-index
+    vncdo
+
+    wireshark
+    traceroute
+
+    pwvucontrol
+
+    rnote
+    krita
+
+    just
+
+    baobab # gnome only?
+
+    #lyx
+
+    libsForQt5.qt5ct
+
+    #filezilla
+    lazygit
+
+    nautilus
+
+    gnome-disk-utility
+    udisks
+
+    # CLI tools
+    ripgrep
+    zoxide
+    fzf
+
+    # Misc
+    playerctl
+    networkmanagerapplet
+    gh # github cli
+    neofetch
+
+    # User applications
+  ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- hosts/voyager/default.nix
+++ hosts/voyager/default.nix
@@ -1,11 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ../common/global
     ../common/users/jakob
 
     # import optional modules
-    ../common/optional/system_packages
     ../common/optional/desktop_environment/wallpaper
     ../common/optional/desktop_environment/window_manager/hyprland
   ];
@@ -16,6 +15,54 @@
   global.xremap.users = ["jakob"];
 
   ###########################################
+
+  environment.systemPackages = with pkgs; [
+    #kicad
+
+    killall
+    usbimager
+
+    # Gnome stuff
+    pomodoro-gtk
+    blanket
+
+    nix-index
+    vncdo
+
+    wireshark
+    traceroute
+
+    pwvucontrol
+
+    rnote
+    krita
+
+    just
+
+    baobab # gnome only?
+
+    libsForQt5.qt5ct
+
+    lazygit
+
+    nautilus
+
+    gnome-disk-utility
+    udisks
+
+    # CLI tools
+    ripgrep
+    zoxide
+    fzf
+
+    # Misc
+    playerctl
+    networkmanagerapplet
+    gh # github cli
+    neofetch
+
+    # User applications
+  ];
 
   system.stateVersion = "24.05";
 }
