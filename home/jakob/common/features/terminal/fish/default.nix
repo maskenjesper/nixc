3a725527d2a55dab1288d94bfc959fef8057cc05
jakob <noreply@email.com>
2024-12-15 11:56:57 +0100

    12 current  2024-12-15 01:13:57  25.05.20241119.23e89b7  6.6.62                          *

--- home/jakob/common/features/terminal/fish/default.nix
+++ home/jakob/common/features/terminal/fish/default.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   home.file.".config/fish" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${builtins.toString ./.}/dotfiles";
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixc/home/jakob/common/features/terminal/fish/dotfiles";
     recursive = true;
   };
 
