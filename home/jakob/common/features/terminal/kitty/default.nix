6d0e5bf3810741735406d98d9deb4c37c8657d20
jakob <noreply@email.com>
2024-12-15 11:20:39 +0100

    12 current  2024-12-15 01:13:57  25.05.20241119.23e89b7  6.6.62                          *

--- home/jakob/common/features/terminal/kitty/default.nix
+++ home/jakob/common/features/terminal/kitty/default.nix
@@ -1,11 +1,12 @@
 {
   pkgs,
   config,
+  outputs,
   ...
 }: {
   home.file.".config/kitty" = {
     # for some reaseon this doesn't work: config.lib.file.mkOutOfStoreSymlink
-    source = "${config.home.homeDirectory}/nixc/home/jakob/common/features/terminal/kitty/dotfiles";
+    source = ./dotfiles;
     recursive = true;
   };
 
