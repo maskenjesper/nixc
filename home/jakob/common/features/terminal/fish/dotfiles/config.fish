2cb779120e6df74dc5f44e5425658260ca7ea64b
jakob <noreply@email.com>
2024-12-19 21:59:02 +0100

    23 current  2024-12-19 13:18:58  25.05.20241119.23e89b7  6.6.62                          *

--- home/jakob/common/features/terminal/fish/dotfiles/config.fish
+++ home/jakob/common/features/terminal/fish/dotfiles/config.fish
@@ -21,17 +21,6 @@ function sudo --description "Replacement for Bash 'sudo !!' command to run last 
 end
 
 bind \co lfcd
-function lfcd --description "lf but it changes directory"
-    set tmp (mktemp)
-    lf --last-dir-path="$tmp" $argv
-    if test -f $tmp
-        set dir (cat $tmp)
-        rm -f $tmp
-        if test -d $dir -a $dir != (pwd)
-            cd $dir
-        end
-    end
-end
 
 bind \ce 'tmux-sessionizer'
 
--- home/jakob/common/features/terminal/tmux/default.nix
+++ home/jakob/common/features/terminal/tmux/default.nix
@@ -38,22 +38,6 @@ in {
       #   # '';
       # }
       tmuxPlugins.vim-tmux-navigator
-      # {
-      #   plugin = tmuxPlugins.resurrect;
-      #   extraConfig = ''
-      #     set -g @resurrect-strategy-vim 'session'
-      #     set -g @resurrect-strategy-nvim 'session'
-      #     set -g @resurrect-capture-pane-contents 'on'
-      #   '';
-      # }
-      # {
-      #   plugin = tmuxPlugins.continuum;
-      #   extraConfig = ''
-      #     set -g @continuum-restore 'on'
-      #     set -g @continuum-boot 'on'
-      #     set -g @continuum-save-interval '10'
-      #   '';
-      # }
     ];
     extraConfig = ''
       set -sg escape-time 0
