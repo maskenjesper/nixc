ec3311f28f3b81b0a9ce9cdcc1dd9a6d6041e1fe
jakob <noreply@email.com>
2024-12-19 23:18:02 +0100

    23 current  2024-12-19 13:18:58  25.05.20241119.23e89b7  6.6.62                          *

--- home/jakob/common/features/terminal/fish/dotfiles/config.fish
+++ home/jakob/common/features/terminal/fish/dotfiles/config.fish
@@ -11,14 +11,6 @@ end
 
 set -U fish_greeting ""
 
-function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
-    if test "$argv" = !!
-        echo sudo $history[1]
-        eval command sudo $history[1]
-    else
-        command sudo $argv
-    end
-end
 
 bind \co lfcd
 
