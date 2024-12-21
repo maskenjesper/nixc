26f7e02d5d1adac280f41fede5c20e19354dc284
jakob <noreply@email.com>
2024-12-21 18:02:53 +0100

    24 current  2024-12-21 11:12:51  25.05.20241119.23e89b7  6.6.62                          *

--- home/jakob/common/features/terminal/nixCats/dotfiles/lua/jakob/core/folding.lua
+++ home/jakob/common/features/terminal/nixCats/dotfiles/lua/jakob/core/folding.lua
@@ -1,7 +1,7 @@
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldcolumn = "0"
-vim.opt.foldtest = ""
+vim.opt.foldtext = ""
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 1
 vim.opt.foldnestmax = 4
