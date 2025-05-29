227e9f79d5874428deff85c6b944501aa0ba9f8d
jakob <noreply@email.com>
2025-05-29 12:58:37 +0200

    22 current  2025-05-29 12:47:14  25.11.20250527.4faa5f5     6.12.30                          *

--- home/jakob/tellus/features/nixCats/dotfiles/lua/jakob/init.lua
+++ home/jakob/tellus/features/nixCats/dotfiles/lua/jakob/init.lua
@@ -1,5 +1,5 @@
 -- Load lze library for lazy-loading
-require("lze").register_handlers(require("lze.x"))
+-- require("lze").register_handlers(require("lze.x"))
 require("lze").register_handlers(require("nixCatsUtils.lzUtils").for_cat)
 
 -- Load modules
