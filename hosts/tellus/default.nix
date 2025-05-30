c06f53f89efd82662395df1f06f581832742ed02
jakob <noreply@email.com>
2025-05-30 22:54:42 +0200

    29 current  2025-05-30 22:54:38  25.11.20250527.4faa5f5     6.12.30                          *

--- hosts/tellus/default.nix
+++ hosts/tellus/default.nix
@@ -12,7 +12,7 @@
     # import optional modules
     ../common/wallpaper.nix
     ../common/desktop_environment/display_manager/sddm
-    ./features/hyprland.nix
+#    ./features/hyprland.nix
     ./features/gaming.nix
     ./features/stylix
   ];
@@ -22,6 +22,14 @@
   ####### define options values here ########
   global.xremap.users = ["jakob"];
   ###########################################
+
+services = {
+  desktopManager.plasma6.enable = true;
+
+  displayManager.sddm.enable = true;
+
+  displayManager.sddm.wayland.enable = true;
+};
 
   swapDevices = lib.mkForce [ ];
 
@@ -40,6 +48,23 @@
   services.hardware.bolt.enable = true;
 
   environment.systemPackages = with pkgs; [
+# KDE
+    kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+    kdePackages.kcalc # Calculator
+    kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+    kdePackages.kcolorchooser # A small utility to select a color
+    kdePackages.kolourpaint # Easy-to-use paint program
+    kdePackages.ksystemlog # KDE SystemLog Application
+    kdePackages.sddm-kcm # Configuration module for SDDM
+    kdiff3 # Compares and merges 2 or 3 files or directories
+    kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+    kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
+    hardinfo2 # System information and benchmarks for Linux systems
+    haruna # Open source video player built with Qt/QML and libmpv
+    wayland-utils # Wayland utilities
+    wl-clipboard # Command-line copy/paste utilities for Wayland
+
+
     #kicad
 
     devenv
