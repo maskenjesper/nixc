5a3fe2c1866fd9bbb58f8661715352ca34515e12
jakob <noreply@email.com>
2025-06-04 17:57:54 +0200

    30 current  2025-06-04 17:57:49  25.11.20250527.4faa5f5     6.12.30                          *

--- home/jakob/tellus/features/stylix/default.nix
+++ home/jakob/tellus/features/stylix/default.nix
@@ -15,7 +15,7 @@
 
     polarity = "dark";
 
-    image = ./Epic-Anime-Awesome-Wallpapers.jpg;
+    image = ./wallpaper.jpg;
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/pandora.yaml";
--- hosts/tellus/default.nix
+++ hosts/tellus/default.nix
@@ -23,14 +23,6 @@
   global.xremap.users = ["jakob"];
   ###########################################
 
-services = {
-  desktopManager.plasma6.enable = true;
-
-  displayManager.sddm.enable = true;
-
-  displayManager.sddm.wayland.enable = true;
-};
-
   swapDevices = lib.mkForce [ ];
 
   environment.etc = {
@@ -48,21 +40,6 @@ services = {
   services.hardware.bolt.enable = true;
 
   environment.systemPackages = with pkgs; [
-# KDE
-    kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
-    kdePackages.kcalc # Calculator
-    kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
-    kdePackages.kcolorchooser # A small utility to select a color
-    kdePackages.kolourpaint # Easy-to-use paint program
-    kdePackages.ksystemlog # KDE SystemLog Application
-    kdePackages.sddm-kcm # Configuration module for SDDM
-    kdiff3 # Compares and merges 2 or 3 files or directories
-    kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
-    kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
-    hardinfo2 # System information and benchmarks for Linux systems
-    haruna # Open source video player built with Qt/QML and libmpv
-    wayland-utils # Wayland utilities
-    wl-clipboard # Command-line copy/paste utilities for Wayland
 
 
     #kicad
