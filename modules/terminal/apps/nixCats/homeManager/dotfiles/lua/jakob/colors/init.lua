b7fc8769e2b580a3cdecb23464286fffa654dd29
maskenjesper <jakobolsson973@gmail.com>
2026-05-02 19:39:51 +0200

    stuff

--- modules/terminal/apps/nixCats/homeManager/dotfiles/lua/jakob/colors/init.lua
+++ modules/terminal/apps/nixCats/homeManager/dotfiles/lua/jakob/colors/init.lua
@@ -1,11 +1,14 @@
 vim.cmd.colorscheme("kanagawa-dragon")
 
--- make background transparent
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-  highlight SignColumn guibg=none
-  highlight SignColumn ctermbg=none
-]]
+if not vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  -- make background transparent
+  vim.cmd [[
+    highlight Normal guibg=none
+    highlight NonText guibg=none
+    highlight Normal ctermbg=none
+    highlight NonText ctermbg=none
+    highlight SignColumn guibg=none
+    highlight SignColumn ctermbg=none
+  ]]
+end
