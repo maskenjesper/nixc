1b99018d9080e74a6950bc9666fc2d265ab976e8
maskenjesper <jakobolsson973@gmail.com>
2026-09-19 00:30:35 +0200

    2026-09-19 00:30:34

--- modules/desktop-environments/noctalia/nixos/default.nix
+++ modules/desktop-environments/noctalia/nixos/default.nix
@@ -16,6 +16,11 @@
     systemd.enable = true;
   };
 
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
+  services.tuned.enable = true;
+  services.upower.enable = true;
+
     environment.systemPackages = with pkgs; [
     ];
 }
