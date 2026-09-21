84cf1012c979cc59ba77a37ffa714e9459f5f42f
Jakob Olsson <jakobolsson973@gmail.com>
2026-09-21 22:50:30 +0200

    2026-09-21 22:41:58

--- modules/feature/ssh/default.nix
+++ modules/feature/ssh/default.nix
@@ -1,18 +1,30 @@
 {
-  flake.nixosModules.ssh = {...}: {
-    # Enable the OpenSSH daemon.
-    services.openssh = {
-      enable = true;
-      ports = [ 6845 ];
-      settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-        PermitRootLogin = "no";
-        AllowUsers = [ "jakob" ];
-      };
+  flake.nixosModules.ssh = {
+    lib,
+    config,
+    ...
+  }: let
+    cfg = config.ssh;
+  in {
+    options.ssh = {
+      enable = lib.mkEnableOption "enables ssh";
     };
 
-    # Ban connections that fail authentication 5 times.
-    services.fail2ban.enable = true;
+    config = lib.mkIf cfg.enable {
+      # Enable the OpenSSH daemon.
+      services.openssh = {
+        enable = true;
+        ports = [6845];
+        settings = {
+          PasswordAuthentication = false;
+          KbdInteractiveAuthentication = false;
+          PermitRootLogin = "no";
+          AllowUsers = ["jakob"];
+        };
+      };
+
+      # Ban connections that fail authentication 5 times.
+      services.fail2ban.enable = true;
+    };
   };
 }
