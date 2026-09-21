3f1c288dcb7e8925d2b3faece9fcdb5672a7de8e
Jakob Olsson <jakobolsson973@gmail.com>
2026-09-21 23:26:18 +0200

    2026-09-21 23:10:54

--- modules/hosts/iso/default.nix
+++ modules/hosts/iso/default.nix
@@ -8,7 +8,28 @@
       localUsers = ["jakob"];
     };
     modules = [
-      self.nixosModules.iso-configuration
+      ({
+        pkgs,
+        modulesPath,
+        ...
+      }: {
+        imports = [
+          "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+        ];
+
+        nixpkgs.hostPlatform = "x86_64-linux";
+
+        nix.settings.experimental-features = [
+          "nix-command"
+          "flakes"
+          "pipe-operators"
+        ];
+
+        environment.systemPackages = with pkgs; [
+          nvim
+          git
+        ];
+      })
     ];
   };
 }
