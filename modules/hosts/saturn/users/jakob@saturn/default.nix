6df3026a07254e919e2ddd1f802553e763448ae2
Jakob Olsson <jakobolsson973@gmail.com>
2026-09-21 23:25:12 +0200

    2026-09-21 23:10:54

--- modules/hosts/saturn/users/jakob@saturn/default.nix
+++ modules/hosts/saturn/users/jakob@saturn/default.nix
@@ -6,7 +6,33 @@
   flake.homeConfigurations."jakob@saturn" = inputs.home-manager.lib.homeManagerConfiguration {
     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
     modules = [
-      self.homeModules.saturn-jakob-configuration
+      ({pkgs, ...}: {
+        imports = with self.homeModules; [
+          niri
+          noctalia
+          fish
+          tmux
+          direnv
+          kitty
+          git
+          gh
+          lazygit
+          neovim
+          just
+        ];
+
+        home.username = "jakob";
+        home.homeDirectory = "/home/jakob";
+
+        home.packages = with pkgs; [
+          keepassxc
+          usbimager
+        ];
+
+        home.stateVersion = "24.05"; # Please read the comment before changing.
+
+        programs.home-manager.enable = true;
+      })
     ];
   };
 }
