3fa752187fcfe875be0c158b5c72968e5fdefc83
Jakob Olsson <jakobolsson973@gmail.com>
2026-09-23 20:06:10 +0200

    2026-09-23 19:13:45

--- modules/flake/root.nix
+++ modules/flake/root.nix
@@ -2,4 +2,15 @@
   imports = [
     inputs.home-manager.flakeModules.home-manager
   ];
+
+  systems = [
+    "x86_64-linux"
+    "x86_64-darwin"
+    "aarch64-linux"
+    "aarch64-darwin"
+  ];
+
+  perSystem = {pkgs, ...}: {
+    formatter = pkgs.alejandra;
+  };
 }
