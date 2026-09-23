acdeb9da05a4218ef89b0272f2fca50c01210d56
Jakob Olsson <jakobolsson973@gmail.com>
2026-09-23 20:52:39 +0200

    2026-09-23 20:45:25

--- modules/feature/kitty/default.nix
+++ modules/feature/kitty/default.nix
@@ -3,65 +3,73 @@
   moduleWithSystem,
   ...
 }: {
-  flake.homeModules.kitty = {pkgs, ...}: {
-    programs.kitty = {
-      enable = true;
-      settings = {
-        confirm_os_window_close = 0;
-        dynamic_background_opacity = true;
-        enable_audio_bell = false;
-        mouse_hide_wait = "-1.0";
-        window_padding_width = 0;
-        background_obacity = "0.9";
-        background_blur = 5;
-        cursor_trail = 1;
-        font_family = "Fira Code";
-        font_size = 15;
-      };
+  flake.homeModules.kitty = {config, ...}: {
+    home.file.".config/kitty" = {
+      # for some reaseon this doesn't work: config.lib.file.mkOutOfStoreSymlink
+      # source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixc/modules/terminal/emulators/kitty/dotfiles";
+      source = ./dotfiles;
+      recursive = true;
     };
   };
-
-  flake.nixosModules.kitty = moduleWithSystem (
-    {self'}: {
-      environment.systemPackages = with self'.packages; [
-        kitty
-      ];
-    }
-  );
-  perSystem = {pkgs, ...}: {
-    packages.kitty = let
-      fira-mono = pkgs.nerd-fonts.fira-mono;
-      fontsConf = pkgs.makeFontsConf {
-        fontDirectories = [fira-mono];
-      };
-    in
-      inputs.wrapper-modules.wrappers.kitty.wrap {
-        inherit pkgs;
-        environment = {
-          "FONTCONFIG_FILE" = "${fontsConf}";
-        };
-        font = {
-          name = "FiraMono Nerd Font Mono";
-          size = 11;
-        };
-        settings = {
-          font_size = 11;
-          scrollbar = "never";
-          pixel_scroll = false;
-          window_padding_width = 9;
-          background_opacity = 0.50;
-          confirm_os_window_close = 0;
-          enable_audio_bell = false;
-          cursor_trail = 1;
-          cursor_trail_start_threshold = 1;
-          cursor_trail_color = "#cba6f7";
-          cursor_shape = "beam";
-          allow_remote_control = true;
-        };
-        keybindings = {
-          "ctrl+backspace" = "send_text all \\x17";
-        };
-        themeFile = "Catppuccin-Mocha";
-      };
-  };
+  # flake.homeModules.kitty = {pkgs, ...}: {
+  #   programs.kitty = {
+  #     enable = true;
+  #     settings = {
+  #       confirm_os_window_close = 0;
+  #       dynamic_background_opacity = true;
+  #       enable_audio_bell = false;
+  #       mouse_hide_wait = "-1.0";
+  #       window_padding_width = 0;
+  #       background_obacity = "0.9";
+  #       background_blur = 5;
+  #       cursor_trail = 1;
+  #       font_family = "Fira Code";
+  #       font_size = 15;
+  #     };
+  #   };
+  # };
+  #
+  # flake.nixosModules.kitty = moduleWithSystem (
+  #   {self'}: {
+  #     environment.systemPackages = with self'.packages; [
+  #       kitty
+  #     ];
+  #   }
+  # );
+  # perSystem = {pkgs, ...}: {
+  #   packages.kitty = let
+  #     fira-mono = pkgs.nerd-fonts.fira-mono;
+  #     fontsConf = pkgs.makeFontsConf {
+  #       fontDirectories = [fira-mono];
+  #     };
+  #   in
+  #     inputs.wrapper-modules.wrappers.kitty.wrap {
+  #       inherit pkgs;
+  #       environment = {
+  #         "FONTCONFIG_FILE" = "${fontsConf}";
+  #       };
+  #       font = {
+  #         name = "FiraMono Nerd Font Mono";
+  #         size = 11;
+  #       };
+  #       settings = {
+  #         font_size = 11;
+  #         scrollbar = "never";
+  #         pixel_scroll = false;
+  #         window_padding_width = 9;
+  #         background_opacity = 0.50;
+  #         confirm_os_window_close = 0;
+  #         enable_audio_bell = false;
+  #         cursor_trail = 1;
+  #         cursor_trail_start_threshold = 1;
+  #         cursor_trail_color = "#cba6f7";
+  #         cursor_shape = "beam";
+  #         allow_remote_control = true;
+  #       };
+  #       keybindings = {
+  #         "ctrl+backspace" = "send_text all \\x17";
+  #       };
+  #       themeFile = "Catppuccin-Mocha";
+  #     };
+  # };
 }
