2abd7d4126f7acebaaef6e7e518f184643813155
Jakob Olsson <jakobolsson973@gmail.com>
2026-09-25 20:35:00 +0200

    2026-09-25 17:22:47

--- modules/features/gh/default.nix
+++ modules/features/gh/default.nix
@@ -1,8 +1,14 @@
 {
-  flake.homeModules.gh = {config, ...}: {
+  flake.homeModules.gh = {
+    config,
+    pkgs,
+    ...
+  }: {
     home.file.".config/gh".source =
       if !config.dotfiles.mutable
       then ./dotfiles
       else config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixc/modules/features/gh/dotfiles";
+
+    home.packages = [pkgs.gh];
   };
 }
