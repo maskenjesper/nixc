723df652c2f241ff53c5be6faf49850f319bb1c7
Jakob Olsson <jakobolsson973@gmail.com>
2026-09-25 22:44:58 +0200

    2026-09-25 17:22:47

--- modules/features/opencode/default.nix
+++ modules/features/opencode/default.nix
@@ -1,9 +1,9 @@
 {
   flake.homeModules.opencode = {config, ...}: {
-    home.file.".config/opencode" = {
-      source = ./dotfiles;
-      recursive = true;
-    };
+    home.file.".config/opencode".source =
+      if !config.dotfiles.mutable
+      then ./dotfiles
+      else config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixc/modules/features/opencode/dotfiles";
 
     programs.opencode = {
       enable = true;
