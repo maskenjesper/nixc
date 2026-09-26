97d24235850ab49bf6498e486379857f80e6c7ae
Jakob Olsson <jakobolsson973@gmail.com>
2026-09-26 17:24:13 +0200

    2026-09-26 17:24:11

--- modules/system/core/boot.nix
+++ modules/system/core/boot.nix
@@ -6,11 +6,13 @@
   flake.nixosModules.boot = {
     pkgs,
     lib,
+    config,
     ...
   }: {
+    imports = [];
     boot = {
       loader = {
-        timeout = 2;
+        timeout = 10;
         efi = {
           canTouchEfiVariables = true;
         };
@@ -21,6 +23,8 @@
         };
         # systemd-boot.enable = true;
       };
+
+      # What is plymouth? It's a graphical boot splash screen manager that hides the kernel messages during boot.
       plymouth = {
         enable = true;
         theme = "catppuccin-mocha";
@@ -31,7 +35,11 @@
           })
         ];
       };
+
+      ### KERNEL ###
       kernelPackages = pkgs.linuxPackages_latest;
+      # If using extra kernel modules, they should be selected from the module set for the kernel version configured above.
+      extraModulePackages = with config.boot.kernelPackages; [];
     };
   };
 }
